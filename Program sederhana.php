3770b40558536fbf53d9152ed72b8441d7f82b0d
Rosita <rositaryz1007@gmail.com>
2017-02-06 20:01:28 +0700

    Add files via upload

---
+++ Program sederhana.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Program Sederhana</title>
+</head>
+<body>
+hello !!! my name is rosita.. this is test page..
+<table width="75%" height="0" border="1" align="center">
+<tr>
+  <td></td>
+	<td></td>
+	<td></td>
+</tr>
+</body>
+</html>
